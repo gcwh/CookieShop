1a33e7ce83a519a5495ffdc82308bdd0b0cb559d
WhaleFall <1640830027@qq.com>
2020-09-14 16:01:10 +0800

    1.1

--- src/dao/OrderDao.java
+++ src/dao/OrderDao.java
@@ -9,37 +9,35 @@ import java.util.*;
 import org.apache.commons.dbutils.handlers.*;
 
 public class OrderDao {
-    public void insertOrder(Connection con, Order order) throws SQLException {
-        QueryRunner r = new QueryRunner();
+    private QueryRunner r;
+    public OrderDao(){
+        r=new QueryRunner(DBUtil.getDataSource());
+    }
+    public void insertOrder(Order order) throws SQLException {
         String sql = "insert into `order`(total,amount,status,paytype,name,phone,address,datetime,user_id) values(?,?,?,?,?,?,?,?,?)";
-        r.update(con,sql,
+        r.update(sql,
                 order.getTotal(),order.getAmount(),order.getStatus(),
                 order.getPaytype(),order.getName(),order.getPhone(),
                 order.getAddress(),order.getDatetime(),order.getUser().getId() );
     }
-    public int getLastInsertId(Connection con) throws SQLException {
-        QueryRunner r = new QueryRunner();
+    public int getLastInsertId() throws SQLException {
         String sql = "select last_insert_id()";
-        BigInteger bi = r.query(con, sql,new ScalarHandler<BigInteger>());
+        BigInteger bi = r.query(sql,new ScalarHandler<BigInteger>());
         return Integer.parseInt(bi.toString());
     }
-    public void insertOrderItem(Connection con, OrderItem item) throws SQLException {
-        QueryRunner r = new QueryRunner();
+    public void insertOrderItem( OrderItem item) throws SQLException {
         String sql ="insert into orderitem(price,amount,goods_id,order_id) values(?,?,?,?)";
-        r.update(con,sql,item.getPrice(),item.getAmount(),item.getGoods().getId(),item.getOrder().getId());
+        r.update(sql,item.getPrice(),item.getAmount(),item.getGoods().getId(),item.getOrder().getId());
     }
     public List<Order> selectAll(int userid) throws SQLException {
-        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
         String sql = "select * from `order` where user_id=? order by datetime desc";
         return r.query(sql, new BeanListHandler<Order>(Order.class),userid);
     }
     public List<OrderItem> selectAllItem(int orderid) throws SQLException{
-        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
         String sql = "select i.id,i.price,i.amount,g.name from orderitem i,goods g where order_id=? and i.goods_id=g.id";
         return r.query(sql, new BeanListHandler<OrderItem>(OrderItem.class),orderid);
     }
     public int getOrderCount(int status) throws SQLException {
-        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
         String sql = "";
         if(status==0) {
             sql = "select count(*) from `order`";
@@ -50,7 +48,6 @@ public class OrderDao {
         }
     }
     public List<Order> selectOrderList(int status, int pageNumber, int pageSize) throws SQLException {
-        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
         if(status==0) {
             String sql = "select o.id,o.total,o.amount,o.status,o.paytype,o.name,o.phone,o.address,o.datetime,u.username from `order` o,user u where o.user_id=u.id order by o.datetime desc limit ?,?";
             return r.query(sql, new BeanListHandler<Order>(Order.class), (pageNumber-1)*pageSize,pageSize );
@@ -60,18 +57,15 @@ public class OrderDao {
         }
     }
     public void updateStatus(int id,int status) throws SQLException {
-        QueryRunner r = new QueryRunner(DBUtil.getDataSource());
         String sql ="update `order` set status=? where id = ?";
         r.update(sql,status,id);
     }
-    public void deleteOrder(Connection con ,int id) throws SQLException {
-        QueryRunner r = new QueryRunner();
+    public void deleteOrder(int id) throws SQLException {
         String sql ="delete from `order` where id = ?";
-        r.update(con,sql,id);
+        r.update(sql,id);
     }
-    public void deleteOrderItem(Connection con ,int id) throws SQLException {
-        QueryRunner r = new QueryRunner();
+    public void deleteOrderItem(int id) throws SQLException {
         String sql ="delete from orderitem where order_id=?";
-        r.update(con,sql,id);
+        r.update(sql,id);
     }
 }
