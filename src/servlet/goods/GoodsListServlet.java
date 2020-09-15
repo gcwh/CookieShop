a3e279aa468ac0f83ac647c154f80f851430aaa9
long0620 <wuxinlong0620@163.com>
2020-09-15 10:39:11 +0800

    1.2  GoodsListServlet

--- src/servlet/goods/GoodsListServlet.java
+++ src/servlet/goods/GoodsListServlet.java
@@ -16,16 +16,47 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "goods_List",urlPatterns = "/goods_list")
+@WebServlet(name = "goods_List", urlPatterns = "/goods_list")
 public class GoodsListServlet extends HttpServlet {
-    private GoodsService gService=new GoodsServiceImpl();
-    private TypeService tService=new TypeServiceImpl();
+    private GoodsService gService = new GoodsServiceImpl();
+    private TypeService tService = new TypeServiceImpl();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = 0;
+        int pageNumber = 0;
+        if (request.getParameter("typeid") != null) {
+            id = Integer.parseInt(request.getParameter("typeid"));
+        }
 
-        request.getRequestDispatcher("/goods_list.jsp").forward(request,response);
+        if (request.getParameter("pageNumber") != null) {
+            pageNumber = Integer.parseInt(request.getParameter("pageNumber"));
+        }
+
+        Type t = null;
+
+        if (id != 0) {
+            t = tService.selectTypeNameByID(id);
+        }
+        request.setAttribute("t", t);
+
+        if (pageNumber <= 0) {
+            pageNumber = 1;
+        }
+        Page p = gService.selectPageByTypeId(id, pageNumber);
+        if (p.getTotalPage() == 0) {
+            p.setTotalCount(1);
+            p.setPageNumber(1);
+        } else {
+            if (pageNumber >= p.getTotalPage() + 1) {
+                p = gService.selectPageByTypeId(id, p.getTotalPage());
+            }
+        }
+        request.setAttribute("p", p);
+        request.setAttribute("id", String.valueOf(id));
+        request.getRequestDispatcher("/goods_list.jsp").forward(request, response);
     }
 }
