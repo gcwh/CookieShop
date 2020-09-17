021de2f150d6da3a4fbf54e7128df666c771a5ca
WhaleFall <1640830027@qq.com>
2020-09-17 19:58:55 +0800

    1.0.1

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,21 +1,14 @@
 package service;
 
-import com.sun.org.apache.xpath.internal.operations.Or;
 import model.Order;
-import model.OrderItem;
+import model.Page;
 
-import java.sql.Connection;
 import java.util.List;
 
 public interface OrderService {
     default void insertOrder( Order order){return;}
-    default void getLastInsertId(){ return; }
-    default void insertOrderItem(OrderItem item){return;}
     default List<Order> selectAll(int uesrid){return null;}
-    default List<OrderItem> selectAllItem(int orderid){return null;}
-    default int getOrderCount(int status){return 0;}
-    default List<Order> selectOrderList(int status,int pageNumber,int pageSize){return null;}
+    default Page getOrderPage(int status,int pageNumber){return null;}
     default void updateStatus(int id,int status){return;}
-    default void deleteOrder(int id){return;}
-    default void deleteOrderItem(int id){return;}
+    default void delete(int id){return;}
 }
